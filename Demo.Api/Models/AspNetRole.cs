7e1797aa8fd87f0309632f7354799851d71c5c58
neoniehimself <neoniequell.ponce@yahoo.com>
2025-09-07 10:16:36 -0400

    Add Comments for Documentation

--- Demo.Api/Models/AspNetRole.cs
+++ Demo.Api/Models/AspNetRole.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
 namespace Demo.Api.Models;
+/// <summary>
+/// This class represents an ASP.NET Core Identity Role with a GUID as the primary key.
+/// </summary>
 public class AspNetRole : IdentityRole<Guid>
 {
     public AspNetRole()
--- Demo.Api/Models/AspNetUser.cs
+++ Demo.Api/Models/AspNetUser.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
 namespace Demo.Api.Models;
+/// <summary>
+/// This class represents an ASP.NET Core Identity User with a GUID as the primary key.
+/// </summary>
 public class AspNetUser : IdentityUser<Guid>
 {
     public AspNetUser()
